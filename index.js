a5b5a0f0f79e9d600e2999c0b3a6c1278f17b8b0
Zachary Larson <Zach@Zacharys-MacBook-Pro.local>
2021-07-06 16:36:14 -0400

    connected to pet boutique database

---
+++ index.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql')
+const { dbconfig } = require('./dbconfig')
+
+const db = mysql.createConnection(dbconfig)
+db.connect()
+db.query('SELECT * FROM customers', (err, rows) => {
+    if(err) {
+        console.error('Error from MySQL: ', err)
+    }
+    console.log('Got back ROWS: ', rows)
+})
+
+db.end()
+
+
